104e90970d0b8216f387ba0e42a5a23c98d615bc
Fgil77 <157411240+Fgil77@users.noreply.github.com>
2024-01-28 23:50:39 +0100

    Añado tabla distancias como objetos

--- Leer_Rejilla_casi_ok.R
+++ Leer_Rejilla_casi_ok.R
@@ -10,6 +10,8 @@ library(dplyr)
 devtools::install_github("r-lib/conflicted")
 
 
+
+
 #repgrid_elementos <- read_excel("C:\\Users\\gilmo\\Desktop\\Ruta_archivos\\Repgrid_LUIS_DAVID_2024-01-09_20_40_23.xlsx")
 #repgrid_elementos
 
@@ -87,38 +89,159 @@ matriz_constructos_d__distancias
 
 matriz_elementos_d__distancias2 <- round(dist(matriz_c_def),2)
 matriz_elementos_d__distancias2
+class(matriz_elementos_d__distancias2)
+
+#----Preparo la tabla de ELEMENTOS distancias
+names(Tabla_e_d) #PARA OBTENER UN VECTOR CON LOS NOMBRES DE LAS VARIBLES / TÍTULOS DE LAS COLUMNAS
+vector_varible_elementos <- names(Tabla_e_d)
+vector_varible_elementos
+#-Montamos la tabla
+matriz_regular_elementos_d_distancias2 <- as.matrix(matriz_elementos_d__distancias2)
+matriz_regular_elementos_d_distancias2
+colnames(matriz_regular_elementos_d_distancias2) <- vector_varible_elementos
+colnames(matriz_regular_elementos_d_distancias2)
+rownames(matriz_regular_elementos_d_distancias2) <- colnames(matriz_regular_elementos_d_distancias2)
+tabla_regular_ELEMENTOS_d_distancias2 <- as.data.frame(matriz_regular_elementos_d_distancias2)
+tabla_regular_ELEMENTOS_d_distancias2
+
+
+#Preparo la tabla de CONSTRUCTOS distancias
+matriz_regular_constructos_d_distancias2 <- as.matrix(matriz_constructos_d__distancias)
+matriz_regular_constructos_d_distancias2
+colnames(matriz_regular_constructos_d_distancias2) <- vector_variables_c
+colnames(matriz_regular_constructos_d_distancias2)
+rownames(matriz_regular_constructos_d_distancias2) <- colnames(matriz_regular_constructos_d_distancias2)
+tabla_regular_CONSTRUCTOS_d_distancias2 <- as.data.frame(matriz_regular_constructos_d_distancias2)
+tabla_regular_CONSTRUCTOS_d_distancias2
+print(tabla_regular_CONSTRUCTOS_d_distancias2)
+
+#---IMPRIMIR LA TABLA COMO OBJETO
+library(gridExtra)
+library(ggplot2)
+
+#creamos tabla con tableGrob
+tabla_image_elementos_distancias <- tableGrob(tabla_regular_ELEMENTOS_d_distancias2)
+tabla_image_elementos_distancias
+#class(tabla_image_elementos_distancias)
+
+#CREAMOS ARCHIVO .PNG
+imagen_dist_ELEMENTOS <- ggsave("Imagen_dist_ELEMENTOS.png", plot = tabla_image_elementos_distancias, device = "png", width = 49, height = 10, units = "in")
+imagen_dist_ELEMENTOS
+
+#otra forma------- CREAR ARCHIVO HTML 
+#install.packages("knitr")
+#install.packages("kableExtra")
+#install.packages("webshot")
+
+
+library(knitr)
+library(kableExtra)
+library(webshot)
+
+tabla_kable_e_dis <- kable(tabla_regular_ELEMENTOS_d_distancias2, "html") %>% kable_styling("striped")
+tabla_kable_e_dis # crea html a partir de la tabla de datos, el styling para dar estilo
+
+temp_html_Elementos <- tempfile(fileext = ".html") 
+temp_html_Elementos
+
+#cat(tabla_kable_e_dis, file = temp_html) #el temporal html en un directorio c C:\\Users\\gilmo\\AppData\\Local\\Temp\\Rtmp6xjOC3\\filec907a7b703d.html
+#la función cat es para concatenar y escribir el contenido en el archivo
+#eliminar tempora html
+#unlink(temp_html)
+
+#install.packages("webshot2") # PARA IMPRMIR COMO IMAGEN EL ARCHIVO
+#library(webshot)
+#library(webshot2)
+#webshot(temp_html,"tabla_imagen2.png", cliprect = "viewport") #(esto no funcionó)
+
+#-----AHORA LO MISMO CON LA TABLA DE CONSTRUCTOS - IMPRIMIR OBJETO CONSTRUCTOS
+#creamos tabla con tableGrob
+tabla_image_constructos_distancias <- tableGrob(tabla_regular_CONSTRUCTOS_d_distancias2)
+tabla_image_constructos_distancias
+#class(tabla_image_constructos_distancias)
+
+#CREAMOS ARCHIVO .PNG
+imagen_dist_CONSTRUCTOS <- ggsave("Imagen_dist_CONSTRUCTOS.png", plot = tabla_image_constructos_distancias, device = "png", width = 49, height = 10, units = "in")
+imagen_dist_CONSTRUCTOS
+
+#otra forma------- CREAR ARCHIVO HTML 
+#install.packages("knitr")
+#install.packages("kableExtra")
+#install.packages("webshot")
+
+
+library(knitr)
+library(kableExtra)
+library(webshot)
+
+tabla_kable_C_dis <- kable(tabla_regular_CONSTRUCTOS_d_distancias2, "html") %>% kable_styling("striped")
+tabla_kable_C_dis # crea html a partir de la tabla de datos, el styling para dar estilo
+
+temp_html_Constructos <- tempfile(fileext = ".html") 
+temp_html_Constructos
+
+#cat(tabla_kable_e_dis, file = temp_html) #el temporal html en un directorio c C:\\Users\\gilmo\\AppData\\Local\\Temp\\Rtmp6xjOC3\\filec907a7b703d.html
+#la función cat es para concatenar y escribir el contenido en el archivo
+#eliminar tempora html
+#unlink(temp_html)
+
+#install.packages("webshot2") # PARA IMPRMIR COMO IMAGEN EL ARCHIVO
+#library(webshot)
+#library(webshot2)
+#webshot(temp_html,"tabla_imagen2.png", cliprect = "viewport") #(esto no funcionó)
+
+
+
+########################################################
+
+
+
+
+
+
+
+#install.packages("openrepgrid") (he intentado utilizar las funciones de opengrid con lo que tengo y nada)
+#library(OpenRepGrid)
+#rep_grid_formato <- repgrid(matriz_c_def, rownames = df$Item, description = "Ejemplo de Repertory Grid")
+#rep_grid_formato (no reconoce la función repgrid)
+
+#install.packages("OpenRepGrid")
+#library(OpenRepGrid)
+
+
+
 
 #desde aqui ya no lo tengo tan claro otras pruebas
 
 # INTENTO SACAR PRIMER FACTOR
-resultado_pca <- prcomp(tabla_c_R, scale. = TRUE)
-primer_factor <- resultado_pca$rotation [,1]
-print("\nprimer factor (componente principal")
-primer_factor
+#resultado_pca <- prcomp(tabla_c_R, scale. = TRUE)
+#primer_factor <- resultado_pca$rotation [,1]
+#print("\nprimer factor (componente principal")
+#primer_factor
 
-representación_lineal <- as.matrix(tabla_c_R) %% primer_factor
-plot(representación_lineal, col = "blue", pch = 19, main = "Representación lineal del primer factor")
+#representación_lineal <- as.matrix(tabla_c_R) %% primer_factor
+#plot(representación_lineal, col = "blue", pch = 19, main = "Representación lineal del primer factor")
 
 # INTENTO SACAR SEGUNDO FACTOR
-resultado_pca2 <- prcomp(tabla_c_R, scale. = TRUE)
-segundo_factor <- resultado_pca2$rotation [,2]
-segundo_factor
+#resultado_pca2 <- prcomp(tabla_c_R, scale. = TRUE)
+#segundo_factor <- resultado_pca2$rotation [,2]
+#segundo_factor
 
-representación_lineal2 <- as.matrix(tabla_c_R) %% segundo_factor
-plot(representación_lineal2, col = "blue", pch = 19, main = "Representación lineal del segundo factor")
+#representación_lineal2 <- as.matrix(tabla_c_R) %% segundo_factor
+#plot(representación_lineal2, col = "blue", pch = 19, main = "Representación lineal del segundo factor")
 
 
 
 #------------GRAFICAS, NO MUY CLARO
-num_conglomerados <- 3
-resultado_kmeans <- kmeans(Tabla_e_d, centers = num_conglomerados)
-df_conglomerados <- cbind(Tabla_e_d, conglomerado = as.factor(resultado_kmeans$cluster))
-df_conglomerados
+#num_conglomerados <- 3
+#resultado_kmeans <- kmeans(Tabla_e_d, centers = num_conglomerados)
+#df_conglomerados <- cbind(Tabla_e_d, conglomerado = as.factor(resultado_kmeans$cluster))
+#df_conglomerados
 #plot(df_conglomerados[,1:2], col = resultado_kmeans$cluster, pch=19, main = "Gráfico de dispersión con conglomerados", xlab = "variable1", ylab "variable2")
-plot(df_conglomerados[,1:3])
+#plot(df_conglomerados[,1:3])
 
 #install.packages("fmsb")
-library(fmsb)
+#library(fmsb)
 #df_conglomerados
 #df_radar <- df_conglomerados[,2:3]
 #df_radar_norm <- as.data.frame(scale(df_radar))
